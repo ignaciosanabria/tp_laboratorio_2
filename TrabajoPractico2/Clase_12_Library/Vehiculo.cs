6aaed3ead5ae84d5ee03d1fea19610b93493b1d2
ignaciosanabria <ignaciosanabria@outlook.com>
2016-10-11 11:56:55 -0300

    Trabajo Practico 2 - Lab II

---
+++ TrabajoPractico2/Clase_12_Library/Vehiculo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library_2
+{
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Yamaha, Chevrolet, Ford, Iveco, Scania, BMW
+        }
+        EMarca _marca;
+        string _patente;
+        ConsoleColor _color;
+        /// <summary>
+        /// Constructor de Vehiculo.
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Vehiculo(EMarca marca, string patente, ConsoleColor color)
+        {
+            this._marca = marca;
+            this._patente = patente;
+            this._color = color;
+        }
+
+       
+       /// <summary>
+       /// Propiedad abstracta de lectura de vehiculo.
+       /// </summary>
+        public abstract short CantidadRuedas
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Metodo virtual que retornara un string con los datos del vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("PATENTE: {0}\r\n", this._patente);
+            sb.AppendFormat("MARCA  : {0}\r\n", this._marca.ToString());
+            sb.AppendFormat("COLOR  : {0}\r\n", this._color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehículos son iguales si comparten la misma patente
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            if (!Object.ReferenceEquals(v1, null) && !Object.ReferenceEquals(v2, null))
+            {
+                if (string.Compare(v1._patente, v2._patente) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Dos vehículos son distintos si su patente es distinta
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+    }
+}
